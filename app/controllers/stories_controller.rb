9210cf0269ea4843c321480455d3989095eac9ad
Poh Kah Kong <kahkong87mail@gmail.com>
2015-06-22 22:22:02 +0800

    Add team buttons

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,34 +2,38 @@ require "pivotal-tracker"
 
 class StoriesController < ApplicationController
   def index
+    @team = params[:team] == "omega" ? "omega" : "bahamut"
+    
     PivotalTracker::Client.token = "4e4499e14f3c63002ce28c692de694c8"
     ida_pivotal = PivotalTracker::Project.find(1345356)
     current_stories = PivotalTracker::Iteration.current(ida_pivotal).stories
-    @unstarted_stories = current_stories.select { |s| planned?(s.current_state)   && bahamut?(s.labels)}
-    @started_stories = current_stories.select   { |s| started?(s.current_state)   && bahamut?(s.labels)}
-    @finished_stories = current_stories.select  { |s| finished?(s.current_state)  && bahamut?(s.labels)}
-    @delivered_stories = current_stories.select { |s| delivered?(s.current_state) && bahamut?(s.labels)}
+    @unstarted_stories = current_stories.select { |s| planned?(s)   && team?(s, @team)}
+    @started_stories = current_stories.select   { |s| started?(s)   && team?(s, @team)}
+    @finished_stories = current_stories.select  { |s| finished?(s)  && team?(s, @team)}
+    @delivered_stories = current_stories.select { |s| delivered?(s) && team?(s, @team)}
+    
+    puts "*** debug *** #{params[:team]}"
   end
   
   private
-    def planned?(current_state)
-      current_state=="planned"
+    def planned?(story)
+      story.current_state=="planned"
     end
 
-    def started?(current_state)
-      current_state=="started"
+    def started?(story)
+      story.current_state=="started"
     end
 
-    def finished?(current_state)
-      current_state=="finished"
+    def finished?(story)
+      story.current_state=="finished"
     end
 
-    def delivered?(current_state)
-      current_state=="delivered"
+    def delivered?(story)
+      story.current_state=="delivered"
     end
   
-    def bahamut?(labels)
-      include_label?(labels, "bahamut")
+    def team?(story, team)
+      include_label?(story.labels, team)
     end
   
     def include_label?(labels, label)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root  'stories#index'
+  get ':team' => 'stories#index'
 end
