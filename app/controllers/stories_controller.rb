67a2c834df2394bf837bd923a34e8721a59ea122
Poh Kah Kong <kahkong87mail@gmail.com>
2015-06-23 22:54:20 +0800

    Refactor stories_controller to stories_helper

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,42 +1,19 @@
 require "pivotal-tracker"
 
 class StoriesController < ApplicationController
+  include StoriesHelper
+  
   def index
-    @team = params[:team] == "omega" ? "omega" : "bahamut"
-    
-    PivotalTracker::Client.token = "4e4499e14f3c63002ce28c692de694c8"
-    ida_pivotal = PivotalTracker::Project.find(1345356)
-    current_stories = PivotalTracker::Iteration.current(ida_pivotal).stories
-    @unstarted_stories = current_stories.select { |s| planned?(s)   && team?(s, @team)}
-    @started_stories = current_stories.select   { |s| started?(s)   && team?(s, @team)}
-    @finished_stories = current_stories.select  { |s| finished?(s)  && team?(s, @team)}
-    @delivered_stories = current_stories.select { |s| delivered?(s) && team?(s, @team)}
-    
-    puts "*** debug *** #{params[:team]}"
+    @team = set_team(params[:team])
+    current_stories = current_stories("4e4499e14f3c63002ce28c692de694c8", 1345356)
+    @unstarted_stories = filter(current_stories, {state: "planned", team: @team})
+    @started_stories =   filter(current_stories, {state: "started",   team: @team})
+    @finished_stories =  filter(current_stories, {state: "finished",  team: @team})
+    @delivered_stories = filter(current_stories, {state: "delivered", team: @team})
   end
-  
+
   private
-    def planned?(story)
-      story.current_state=="planned"
-    end
-
-    def started?(story)
-      story.current_state=="started"
-    end
-
-    def finished?(story)
-      story.current_state=="finished"
-    end
-
-    def delivered?(story)
-      story.current_state=="delivered"
-    end
-  
-    def team?(story, team)
-      include_label?(story.labels, team)
-    end
-  
-    def include_label?(labels, label)
-      !labels.nil? && labels.include?(label)
+    def set_team(team)
+      team == "omega" ? "omega" : "bahamut"
     end
 end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,20 @@
 module StoriesHelper
+  def current_stories(token, project_id)
+    PivotalTracker::Client.token = token
+    project = PivotalTracker::Project.find(project_id)
+    PivotalTracker::Iteration.current(project).stories
+  end
+  
+  def filter(stories, options = {})
+    team = options[:team]
+    state = options[:state]
+    stories.select do |story|
+      (state.nil? || story.current_state==state) &&
+      (team.nil? || include_label?(story, team))
+    end
+  end
+
+  def include_label?(story, label)
+    !story.labels.nil? && story.labels.include?(label)
+  end
 end
