16a6d25c5adf1546a65b09785b02bec5570c5c7a
Poh Kah Kong <kahkong87mail@gmail.com>
2015-07-08 23:26:51 +0800

    Add teams to application.yml and header view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :teams
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   include PivotalApiHelper
+  
+  def teams
+    @teams = ENV["TEAMS"].split(",")
+  end
 
   protected
     def set_team(team)
-      team == "omega" ? "omega" : "bahamut"
+      @teams.include?(team)?team:@teams[0]
     end
 end
