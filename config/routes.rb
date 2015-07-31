ddad590528085c3ebd23c33336cd93cd52eca91c
Poh Kah Kong <kahkong87mail@gmail.com>
2015-07-31 23:16:37 +0800

    Refactor id for views

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   get '/board'         => 'board#index'
   get '/board/:team'   => 'board#index'
   get '/chart/:team'   => 'chart#index'
-  get '/pairing/:team' => 'pairing#index'
   get '/list/:team'    => 'list#index'
 end
