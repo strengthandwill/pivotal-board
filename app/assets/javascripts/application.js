397637ba5567be6bb68d4c5c7a76dc5fad546883
Poh Kah Kong <kahkong87mail@gmail.com>
2015-08-03 09:54:38 +0800

    Fix modal not coming up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require highcharts/highstock
 //= require highcharts/highcharts                                                           
 //= require highcharts/highcharts-more   
