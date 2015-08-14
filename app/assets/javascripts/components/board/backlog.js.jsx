a594d16aaf6552b626ab5a9ed13e74e1df5ac638
Poh Kah Kong <kahkong87mail@gmail.com>
2015-08-14 08:36:50 +0800

    Add story points to backlog header

--- app/assets/javascripts/components/board/backlog.js.jsx
+++ app/assets/javascripts/components/board/backlog.js.jsx
@@ -2,7 +2,10 @@ var Backlog = React.createClass({
   render: function() {
     return (
       <div className="backlog panel panel-default col-md-2">
-        <div className="panel-heading">{this.props.name}</div>
+        <div className="panel-heading">
+          <span>{this.props.name}</span>
+          <span className="pull-right">{this.props.story_points}</span>
+        </div>
         <div className="panel-content">
           {this.props.stories != null && this.props.stories.map(function(story) {
             return <Story story={story} />;
--- app/assets/javascripts/components/board/board.js.jsx
+++ app/assets/javascripts/components/board/board.js.jsx
@@ -2,12 +2,24 @@ var Board = React.createClass({
   render: function() {
     return (
       <div className="board">
-        <Backlog name="DEV Todo" stories={this.props.data.unstarted_stories} />
-        <Backlog name="DEV In Progress" stories={this.props.data.started_stories} />
-        <Backlog name="QE Testing" stories={this.props.data.finished_stories} />
-        <Backlog name="PPO Acceptance" stories={this.props.data.delievered_stories} />
-        <Backlog name="Impeded" stories={this.props.data.impeded_stories} />
-        <Backlog name="Done" stories={this.props.data.accepted_stories}/>
+        <Backlog name="DEV Todo" 
+                 stories={this.props.data.unstarted_stories} 
+                 story_points={this.props.data.unstarted_story_points} />
+        <Backlog name="DEV In Progress" 
+                 stories={this.props.data.started_stories}
+                 story_points={this.props.data.started_story_points} />
+        <Backlog name="QE Testing" 
+                 stories={this.props.data.finished_stories}
+                 story_points={this.props.data.finished_story_points} />
+        <Backlog name="PPO Acceptance" 
+                 stories={this.props.data.delievered_stories}
+                 story_points={this.props.data.delivered_story_points} />
+        <Backlog name="Impeded" 
+                 stories={this.props.data.impeded_stories}
+                 story_points={this.props.data.impeded_story_points} />
+        <Backlog name="Done" 
+                 stories={this.props.data.accepted_stories}
+                 story_points={this.props.data.accepted_story_points} />
       </div>
     );
   }
