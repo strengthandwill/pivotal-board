64c121f983d73b3be6092e8443cd973385e62ea4
Poh Kah Kong <kahkong87mail@gmail.com>
2018-05-26 21:41:14 +0800

    Update ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_person!
   before_action :set_accounts
@@ -7,31 +8,29 @@ class ApplicationController < ActionController::Base
   include PivotalApiHelper
 
   protected
-
-  def set_accounts
-    @accounts = Account.all
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |u|
-      u.permit(:username,
-               :password,
-               :remember_me)
+    def set_accounts
+      @accounts = Account.all
     end
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:username,
-               :name,
-               :email,
-               :password,
-               :password_confirmation,
-               :current_password,
-               :image_path)
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in) do |u|
+        u.permit(:username,
+                 :password,
+                 :remember_me)
+      end
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:username,
+                 :name,
+                 :email,
+                 :password,
+                 :password_confirmation,
+                 :current_password,
+                 :image_path)
+      end
     end
-  end
 
   private
-
-  def settings
-    Setting.last
-  end
+    def settings
+      Setting.last
+    end
 end
